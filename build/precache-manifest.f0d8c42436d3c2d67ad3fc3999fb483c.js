c0067502c5d12f694007a4acbcd1746c878a98f7
mtisikli <mtisikli@gmail.com>
2019-01-07 20:41:36 +0200

    eee

---
+++ build/precache-manifest.f0d8c42436d3c2d67ad3fc3999fb483c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac52e8e49a51c41c574a",
+    "url": "/static/js/main.ac52e8e4.chunk.js"
+  },
+  {
+    "revision": "1169134135fbc7906a23",
+    "url": "/static/js/1.11691341.chunk.js"
+  },
+  {
+    "revision": "ac52e8e49a51c41c574a",
+    "url": "/static/css/main.9248ddb3.chunk.css"
+  },
+  {
+    "revision": "b1a53fc81812b2473f0d808efea8e7e4",
+    "url": "/index.html"
+  }
+];
